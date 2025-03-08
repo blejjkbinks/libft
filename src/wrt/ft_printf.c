ab642e79547f7083717ba37accba43a166e6a272
blejj k binks <romitdomange@gmail.com>
2025-03-08 16:13:38 +1030

    p-1

--- src/wrt/ft_printf.c
+++ src/wrt/ft_printf.c
@@ -149,9 +149,9 @@ static char	*ft_printf_elem(va_list *a, const char **fmt, int *flg, char *e)
 
 static char	*ft_printf_trns(char *e, int *flg, size_t w, size_t p)
 {
-	if (flg[3] && flg['s'] && p >= 0 && ft_strlen(e) > p - 1)
+	if (flg[3] && flg['s'] && p - 1> 0 && ft_strlen(e) > p - 1)
 		e[flg[2] - 1] = 0;
-	while (flg[3] && !flg['s'] && p >= 0 && ft_strlen(e) < p - 1)
+	while (flg[3] && !flg['s'] && p - 1 >= 0 && ft_strlen(e) < p - 1)
 		e = ft_strjoin("0", e) + (long)ft_free(e);
 	if (flg['p'] || (flg['#'] && ft_strchr("xX", flg[4])))
 		e = ft_strjoin("0x", e) + (long)ft_free(e);
